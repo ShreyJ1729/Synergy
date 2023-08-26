491794a5b97c72bd23372d4cc3039f73a4c1d54b
Shrey Joshi <shreyjoshi2004@gmail.com>
2023-08-26 16:44:10 -0400

    add 2 backend endpoints

--- backend/common.py
+++ backend/common.py
@@ -1,3 +1,26 @@
-from modal import Stub
+from modal import Stub, Image
+import modal
 
-stub = Stub(name="synergy")
+mounts = [
+    modal.Mount.from_local_file(
+        ".env",
+        "/root/.env",
+    ),
+]
+
+image = (
+    modal.Image.debian_slim()
+    .pip_install_from_requirements("requirements.txt")
+    .run_commands(
+        "apt-get update",
+        "apt-get install -y git",
+        "cd /root && git clone https://github.com/ggerganov/llama.cpp",
+        "cd /root/llama.cpp && make",
+    )
+)
+
+stub = Stub(
+    image=image,
+    mounts=mounts,
+    name="synergy",
+)
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@
 Main web application service. Serves the static frontend as well as
 API routes for transcription, language model generation and text-to-speech.
 """
-
 import json
 from pathlib import Path
 import modal
@@ -21,7 +20,30 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 chat_contexts = {}
 
-async def determine_response():
+
+@stub.function(
+    container_idle_timeout=60,
+    timeout=60,
+)
+@modal.web_endpoint(method="POST")
+async def determine_response(request: Request):
+    id = request.query_params["id"]
+    response = (
+        openai.ChatCompletion.create(
+            model="gpt-4-0613",
+            messages=chat_contexts[id],
+            temperature=0.75,
+            max_tokens=250,
+            top_p=1,
+            frequency_penalty=0.22,
+            presence_penalty=0.21,
+        )
+        .choices[0]
+        .message.content
+    )
+
+    return response
+
 
 @stub.function(
     cpu=12,
@@ -41,24 +63,13 @@ async def transcribe(request: Request):
 
     result = transcriber.transcribe_segment.call(bytes)
     if id not in chat_contexts:
-        chat_contexts[id] = [{
+        chat_contexts[id] = [
+            {
                 "role": "system",
                 "content": '- You speak like a charismatic human, NOT A HELPFUL AI ASSISTANT.  You respond the way humans verbally converse.\n- You are direct, concise, but approachable and fun sometimes. You get straight to the point and help move the discussion forward with the user. Refrain from meaningless sentences that an assistant would say, like "I see you\'re trying to <X>." No human to human conversation goes like that.\n- Talk like humans, the way they lead people into following their thoughts with phrases like "right?" and "so,", and similarly persuasive but concise wording.\n- You are not a text based AI assistant, you are an expert at small-talk and respond in human conversation. This is NOT a text message experience, this is a verbal conversation.\n- Important! You are in the middle of a live conversation with a human, and will have to start thinking of your response even if you don\'t have the full sentence from the human.  Deduce where their thought is going, and start constructing your response to them. Identify the persons mood, attitude, and likely direction of conversation. Specify if you think we should interject with should_we_speak based on how confident you are in the context of conversation. If you don\'t think we have enough information yet, KEEP LISTENING and respond with should_we_speak: false. BE SURE you have key features / entities / attributes of what the person is talking about before speaking. We don\'t want to make a meaningless conversation.\nStructure your responses as such:\n{"should_we_speak":"true/false", "generated": "<your response to user>"}',
-            }]
-    
+            }
+        ]
+
     chat_contexts[id].append({"role": "user", "content": result["text"]})
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4-0613",
-        messages=chat_contexts[id],
-        temperature=0.75,
-        max_tokens=250,
-        top_p=1,
-        frequency_penalty=0.22,
-        presence_penalty=0.21,
-    )
-
-    return {
-        "transcribed_text": result["text"],
-        "generated_text": response["choices"][0]["text"],
-    }
+    return result["text"]
